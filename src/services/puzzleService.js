2557c998527b1d7b460be7bfd4566ce39c0c5a63
Mark Aziz <v-markaziz@microsoft.com>
2024-11-02 01:54:00 +0200

    v6.3 - manage assets

--- src/services/puzzleService.js
+++ src/services/puzzleService.js
@@ -16,7 +16,7 @@ const puzzles = [
   { id: 3, type: 'text', next: 1, data: { clue: 'Hosea [ 15 * 2 / 3 + 1 : 1]', answer: 'egypt', rooms: 'the roof' } },
   { id: 4, type: 'text', next: 1, data: { clue: 'A Biblical city known as the City of David', answer: 'jerusalem', rooms: '108 & 109' } },
   { id: 5, type: 'image', next: 0, data: { imageUrl: '/clues/france.jpg', answer: 'france', rooms: '308 & 309' } },
-  { id: 6, type: 'image', next: 1, data: { imageUrl: '/clues/spain.jpg', answer: 'spain', rooms: '302' } },
+  { id: 6, type: 'image', next: 1, data: { imageUrl: '/clues/spain.png', answer: 'spain', rooms: '302' } },
   { id: 7, type: 'image', next: 0, data: { imageUrl: '/clues/mexico.jpg', answer: 'mexico', rooms: '102 & 103' } },
   { id: 8, type: 'text', next: 1, data: { clue: 'Acts 17:16', answer: 'greece', rooms: '303 & 304' } },
   { id: 9, type: 'text', next: 0, data: { clue: 'A land of samba and carnival glee, I am known for my coffee and soccer, you see', answer: 'brazil', rooms: '104 & 105' } },
